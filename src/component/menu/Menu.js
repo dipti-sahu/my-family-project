ddce90eeb62f168871820614b07be13185ca5a6b
dipti-sahu <diptisahu537@.com>
2023-09-19 20:00:39 +0530

    family project added

--- src/component/menu/Menu.js
+++ src/component/menu/Menu.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export const Menu = () => {
   return (
-    <div>
+    <div className={style.nav}>
        <ul className={style.menu}>
         <li><u><Link href='/home'>Home</Link></u></li>
         <li ><u><Link href='/about'>About</Link></u></li>
