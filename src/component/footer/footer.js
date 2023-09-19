d90bd2d1cdd70109704b8313ef02c8f300fdbfde
dipti-sahu <diptisahu537@.com>
2023-09-19 18:24:24 +0530

    family project successfully completed

---
+++ src/component/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={`text-center bg-primary ${style.footer}`}>
+      Small family happy family
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/component/menu/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './menu.module.css'
+import Link from 'next/link'
+
+export const Menu = () => {
+  return (
+    <div>
+       <ul className={style.menu}>
+        <li><u><Link href='/home'>Home</Link></u></li>
+        <li ><u><Link href='/about'>About</Link></u></li>
+        <li><u><Link href='/contact'>Contact</Link></u></li>
+      </ul>
+     
+    </div>
+  );
+}
+
